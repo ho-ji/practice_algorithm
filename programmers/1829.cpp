8f06fc700e6be586ab481431a2d37cdb0716cd1e
ho-ji <wkdd0205@gmail.com>
2022-03-05 22:50:58 +0900

    Update 1829.cpp
    1829 카카오프렌즈 컬러링북 문제풀이_update(재귀 -> queue사용)

--- programmers/1829.cpp
+++ programmers/1829.cpp
@@ -1,40 +1,40 @@
 #include <vector>
-
+#include <queue>
 using namespace std;
-vector<vector<int>> p;
-int cnt;
-void check(int x, int y, int c){
-    int dx[4] = {0,1,0,-1};
-    int dy[4] = {1,0,-1,0};
-    for(int i=0; i<4; i++){
-        int nx = x+dx[i];
-        int ny = y+dy[i];
-        if(nx>=0&&nx<p.size()&&ny>=0&&ny<p[0].size()){
-            if(p[nx][ny] == c){
-                cnt++;
-                p[nx][ny] = 0;
-                check(nx,ny,c);
-            }
-        }
-    }       
-}
 
 // 전역 변수를 정의할 경우 함수 내에 초기화 코드를 꼭 작성해주세요.
+
 vector<int> solution(int m, int n, vector<vector<int>> picture) {
     int number_of_area = 0;
     int max_size_of_one_area = 0;
-    
-    p.assign(m,vector<int>(n));
-    copy(picture.begin(), picture.end(), p.begin());
-    
+    queue<pair<int,int>> q;
+    int dx[4] = {0,1,0,-1};
+    int dy[4] = {1,0,-1,0};
+
     for(int i=0; i<m; i++){
         for(int j=0; j<n; j++){
-            if(p[i][j] != 0){
-                cnt = 0;
-                check(i,j,p[i][j]);
-                if(cnt > max_size_of_one_area)
-                    max_size_of_one_area = cnt;
+            if(picture[i][j] != 0){
                 ++number_of_area;
+                int cnt = 0;
+                int paint_color = picture[i][j];
+                picture[i][j]=0;
+                q.push(make_pair(i,j));
+                while(!q.empty()){
+                    ++cnt;
+                    int x= q.front().first;
+                    int y= q.front().second;
+                    q.pop();
+                    for(int k=0; k<4; k++){
+                        int nx = x+dx[k];
+                        int ny = y+dy[k];
+                        if(nx>=0 && nx<m && ny>=0 && ny<n && picture[nx][ny] == paint_color){
+                            picture[nx][ny] = 0;
+                            q.push(make_pair(nx,ny));
+                        }
+                    }
+                }
+                if(max_size_of_one_area < cnt)
+                    max_size_of_one_area = cnt;
             }
         }
     }
