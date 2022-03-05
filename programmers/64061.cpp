fa45eb82032e9a5c95d8e3082c5d3ed2a0072814
ho-ji <wkdd0205@gmail.com>
2022-03-05 21:12:48 +0900

    Update 64061.cpp
    64061 크레인 인형뽑기 게임 문제풀이_update

--- programmers/64061.cpp
+++ programmers/64061.cpp
@@ -5,38 +5,24 @@
 using namespace std;
 
 int solution(vector<vector<int>> board, vector<int> moves) {
-    int answer = 0;
-    int size = board.size();
-    vector<int> pos(size+1,-1);
-    stack<int> s;
-    int cnt = 0;
-    for(int i=0; i<size; i++){
-        for(int j=0; j<size; j++){
-            if(pos[j] == -1&&board[i][j] !=0){
-                pos[j] = i;
-                ++cnt;
-            }
-        }
-        if(cnt == size)
-            break;
-    }
-    for(int i=0; i<moves.size(); i++){
-        int m = moves[i] -1;
-        if(pos[m] != -1){
-            if(s.empty())
-                s.push(board[pos[m]][m]);
-            else{
-                if(s.top() == board[pos[m]][m]){
-                    s.pop();
-                    answer +=2;
-                }
-                else
-                    s.push(board[pos[m]][m]);
-            }
-            pos[m]++;
-            if(pos[m]==size)
-                pos[m] = -1;
-        }
-    }
-    return answer;
+	int answer = 0;
+	stack<int> s;
+	for(int i=0; i <moves.size(); i++){
+		int m = moves[i] - 1;
+		for(int j=0; j<board.size(); j++){
+			if(board[j][m] != 0){
+				if(!s.empty() && s.top() == board[j][m]){
+					s.pop();
+					answer += 2;
+				}
+				else{
+					s.push(board[j][m]);
+				}
+				board[j][m] = 0;
+				break;
+			}
+		}
+	}    
+
+	return answer;
 }
