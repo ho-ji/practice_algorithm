801746ba20a85d0f9ff84ee492814f2aaa76d241
ho-ji <wkdd0205@gmail.com>
2022-03-11 23:14:44 +0900

    Update 1835.cpp
    1835 단체사진 찍기_update(loop횟수 줄임. next_permutaion do_while문에 구현)

--- programmers/1835.cpp
+++ programmers/1835.cpp
@@ -1,44 +1,33 @@
 #include <string>
 #include <vector>
 #include <algorithm>
-#include <iostream>
 
 using namespace std;
 
 // 전역 변수를 정의할 경우 함수 내에 초기화 코드를 꼭 작성해주세요.
 int solution(int n, vector<string> data) {
     int answer = 0;
+    int i;
     string c = "ACFJMNRT";
-    vector<string> list;
     do{
-        list.push_back(c);
-    }while(next_permutation(c.begin(),c.end()));
-    for(int i=0; i < list.size(); i++){
-        bool flag = true;
-        for(int j=0; j<n; j++){
-            int diff = list[i].find(data[j][0]) - list[i].find(data[j][2]) ;
-            diff = abs(diff) -1;
-            if(data[j][3] == '='){
-                if(diff != (data[j][4]-'0')){
-                    flag = false;
+        for(i=0; i<data.size(); i++){
+            int diff = c.find(data[i][0]) - c.find(data[i][2]);
+            diff = abs(diff) - 1;
+            if(data[i][3]=='='){
+                if(diff != data[i][4]-'0')
                     break;
-                }
             }
-            else if(data[j][3] == '<'){
-                if(diff >= (data[j][4]-'0')){
-                    flag = false;
+            else if(data[i][3] == '>'){
+                if(diff <= data[i][4]-'0')
                     break;
-                }
             }
-            else if(data[j][3] == '>'){
-                if(diff <= (data[j][4]-'0')){
-                    flag = false;
+            else if(data[i][3] == '<'){
+                if(diff >= data[i][4] - '0')
                     break;
-                }
             }
         }
-        if(flag == true)
+        if(i==data.size())
             answer++;
-    }
+    }while(next_permutation(c.begin(), c.end()));
     return answer;
 }
