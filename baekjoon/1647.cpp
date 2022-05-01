7a90534b390d17ddf77cce3624c575571b709500
ho-ji <wkdd0205@gmail.com>
2022-05-02 00:17:32 +0900

    1647번 도시 분할 계획 문제풀이
    c++, 최소 스패닝 트리

---
+++ baekjoon/1647.cpp
@@ -0,0 +1,64 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+vector<pair<int, pair<int, int>>> edge;
+vector<int> parent;
+
+int find_root(int x){
+	if(parent[x] == x)
+		return x;
+	else
+		return parent[x] = find_root(parent[x]);
+}
+
+void union_root(int x, int y){
+	x = find_root(x);
+	y = find_root(y);
+
+	if(x != y){
+		parent[y] = x;
+	}
+}
+
+int main(){
+	int n,m;
+
+	cin >> n >> m;
+
+	for(int i=0; i<n; i++){
+		parent.push_back(i);
+	}
+
+	int a,b,c;
+
+	for(int i=0; i<m; i++){
+		cin >> a >> b >> c;
+		edge.push_back(make_pair(c,make_pair(a-1, b-1)));
+	}
+
+	sort(edge.begin(), edge.end());
+
+	int count = 0, answer = 0;
+	for(int i=0; i < edge.size(); i++){
+		a = edge[i].second.first;
+		b = edge[i].second.second;
+		c = edge[i].first;
+
+		if(find_root(a) != find_root(b)){
+			answer += c;
+			union_root(a,b);
+			count ++;
+			if(count == n-1){
+				answer -= c;
+				break;
+			}
+		}
+	}
+
+	cout << answer;
+
+	return 0;
+}
