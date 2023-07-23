47893777af1c376d6733cfa7caecad9ea28a19af
ho-ji <wkdd0205@gmail.com>
2023-07-23 21:12:43 +0900

    개인정보 수집 유효기간 문제풀이 수정
    개인정보 수집 유효기간 문제풀이 수정

--- Javascript/programmers/150370.js
+++ Javascript/programmers/150370.js
@@ -1,15 +1,18 @@
 function solution(today, terms, privacies) {
   let termList = {};
   let answer = [];
+
   for (const term of terms) {
     const [a, b] = term.split(" ");
     termList[a] = parseInt(b);
   }
+
+  const [endYear, endMonth, endDay] = today.split(".").map(Number);
+  const endDate = endYear * 12 * 28 + endMonth * 28 + endDay;
+
   for (let i = 1; i <= privacies.length; i++) {
     const [date, term] = privacies[i - 1].split(" ");
     const [startYear, startMonth, startDay] = date.split(".").map(Number);
-    const [endYear, endMonth, endDay] = today.split(".").map(Number);
-    const endDate = endYear * 12 * 28 + endMonth * 28 + endDay;
     const startDate = startYear * 12 * 28 + startMonth * 28 + startDay;
     if (termList[term] <= (endDate - startDate) / 28) {
       answer.push(i);
