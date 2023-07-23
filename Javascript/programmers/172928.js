71765b9f1be1f29493646784e2ba76e34bdbb84f
ho-ji <wkdd0205@gmail.com>
2023-07-24 01:05:00 +0900

    공원산책 문제풀이 수정
    공원산책 문제풀이 수정

--- Javascript/programmers/172928.js
+++ Javascript/programmers/172928.js
@@ -16,8 +16,8 @@ function solution(park, routes) {
     let [d, n] = route.split(" ");
     n = parseInt(n);
     for (var i = 1; i <= n; i++) {
-      var nx = x + moves[d][0] * n;
-      var ny = y + moves[d][1] * n;
+      var nx = x + moves[d][0] * i;
+      var ny = y + moves[d][1] * i;
       if (nx >= 0 && nx < park.length && ny >= 0 && ny < park[0].length) {
         if (park[nx][ny] === "X") {
           break;
