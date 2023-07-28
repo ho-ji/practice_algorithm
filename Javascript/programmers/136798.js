a00d223d17d2f273736f1c956be6a92179b87970
ho-ji <wkdd0205@gmail.com>
2023-07-28 15:13:30 +0900

    기사단원의 무기 수정
    기사단원의 무기 수정

--- Javascript/programmers/136798.js
+++ Javascript/programmers/136798.js
@@ -1,6 +1,6 @@
 function solution(number, limit, power) {
   let result = 1
-  for (let n = 1; n <= number; n++) {
+  for (let n = 2; n <= number; n++) {
     let count = 0
     for (let i = 1; i * i <= n; i++) {
       if (n % i === 0) {
