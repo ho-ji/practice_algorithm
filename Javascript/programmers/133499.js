ebbba0d7df8e5476eac682f9edb4a3f33d6b1616
ho-ji <wkdd0205@gmail.com>
2023-08-02 01:08:58 +0900

    옹알이(2) 문제풀이 수정
    옹알이(2) 문제풀이 수정

--- Javascript/programmers/133499.js
+++ Javascript/programmers/133499.js
@@ -1,11 +1,8 @@
 function solution(babbling) {
   let answer = 0
-  for (let i = 0; i < babbling.length; i++) {
-    if (babbling[i] === 'a') {
-      if (i < babbling.length - 2 && babbling[i + 1] == 'y' && babbling[i + 2] == 'a') {
-        i += 2
-      }
-    }
+  const regex = /^(aya(?!aya)|ye(?!ye)|woo(?!woo)|ma(?!ma))+$/
+  for (const b of babbling) {
+    if (b.match(regex)) answer++
   }
   return answer
 }
