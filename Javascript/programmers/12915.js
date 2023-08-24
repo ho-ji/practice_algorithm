b66d6b4e601c9b1c0b29755c3ba569fdd34d6724
ho-ji <wkdd0205@gmail.com>
2023-08-24 23:08:38 +0900

    문자열 내 마음대로 정렬하기 문제풀이 수정
    문자열 내 마음대로 정렬하기 문제풀이 수정

--- Javascript/programmers/12915.js
+++ Javascript/programmers/12915.js
@@ -1,8 +1,8 @@
 function solution(strings, n) {
   return strings.sort((a, b) => {
     if (a[n] === b[n]) {
-      return b > a ? 1 : -1
+      return b < a ? 1 : -1
     }
-    return b[n] > a[n] ? 1 : -1
+    return b[n] < a[n] ? 1 : -1
   })
 }
