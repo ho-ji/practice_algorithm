93efa7a6c4d455fc63e857acd677a2c3dd440ffa
ho-ji <wkdd0205@gmail.com>
2023-09-22 23:11:06 +0900

    다음 큰 숫자 문제풀이

---
+++ Javascript/programmers/12911.js
@@ -0,0 +1,6 @@
+function solution(n) {
+  let count = [...n.toString(2)].filter((x) => x === '1').length
+  let number = n + 1
+  while ([number.toString(2)].filter((x) => x === '1').length !== count) number++
+  return number
+}
