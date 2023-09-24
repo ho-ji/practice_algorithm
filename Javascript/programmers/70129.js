e2c919dfd239fd304b71363670fd1394e77d551b
ho-ji <wkdd0205@gmail.com>
2023-09-25 00:17:25 +0900

    이진 변환 반복하기 문제풀이

--- Javascript/programmers/70129.js
+++ Javascript/programmers/70129.js
@@ -2,11 +2,9 @@ function solution(s) {
   let count = [0, 0]
   while (s !== 1) {
     count[1] += [...s.toString(2)].length
-    s = [...s.toString(2)].filter((x) => x === '1')
-    count[1] -= s.length
+    s = [...s.toString(2)].filter((x) => x === '1').length
+    count[1] -= s
     count[0]++
-    s = parseInt([...s], 2)
-    console.log(s)
   }
   return count
 }
