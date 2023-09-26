929e7190325caaa65df2a1e5efea6689f23ebf4b
ho-ji <wkdd0205@gmail.com>
2023-09-26 16:39:53 +0900

    다음 큰 숫자 문제풀이 수정

--- Javascript/programmers/12911.js
+++ Javascript/programmers/12911.js
@@ -1,6 +1,6 @@
 function solution(n) {
   let count = [...n.toString(2)].filter((x) => x === '1').length
   let number = n + 1
-  while ([number.toString(2)].filter((x) => x === '1').length !== count) number++
+  while ([...number.toString(2)].filter((x) => x === '1').length !== count) number++
   return number
 }
