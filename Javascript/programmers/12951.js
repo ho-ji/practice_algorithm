72d9cbd505583f095cda2a6404905bc0d249e088
ho-ji <wkdd0205@gmail.com>
2023-10-11 01:22:44 +0900

    JadenCase 문자열 만들기 문제풀이 수정

--- Javascript/programmers/12951.js
+++ Javascript/programmers/12951.js
@@ -1,14 +1,7 @@
 function solution(s) {
-  const words = s.split(' ')
-
-  for (let i = 0; i < words.length; i++) {
-    if (words[i][0] >= 'A' && words[i][0] <= 'Z') {
-      words[i] = words[i][0] + words[i].toLowerCase().slice(1)
-    } else if (words[i][0] >= 'a' && words[i][0] <= 'z') {
-      words[i] = String(words[i][0]).toUpperCase() + words[i].toLowerCase().slice(1)
-    } else {
-      words[i] = words[i].toLowerCase()
-    }
+  let answer = s[0].toUpperCase()
+  for (let i = 1; i < s.length; i++) {
+    answer += s[i - 1] === ' ' ? s[i].toUpperCase() : s[i].toLowerCase()
   }
-  return words.join(' ')
+  return answer
 }
